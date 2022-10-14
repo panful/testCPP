3e5e5ad00264c66b1df6176c9ddfe9dd3fb85125
YangPan <yangpan0822@qq.com>
2022-10-14 11:40:53 +0800

    @edit pthread

--- source/00_CPP_TEST/04_Thread/main.cpp
+++ source/00_CPP_TEST/04_Thread/main.cpp
@@ -20,9 +20,10 @@
 19.
 20.
 21.线程池
+22.pthread
 */
 
-#define TEST5
+#define TEST22
 
 
 #ifdef TEST0
@@ -993,3 +994,72 @@ int main()
 }
 
 #endif // TEST21
+
+#ifdef TEST22
+
+#ifdef __GNUC__
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <pthread.h>
+
+#define MAX 50
+// 全局变量
+int number;
+
+// 线程处理函数
+void* funcA_num(void* arg)
+{
+    for(int i=0; i<MAX; ++i)
+    {
+        int cur = number;
+        cur++;
+        usleep(10);
+        number = cur;
+        printf("Thread A, id = %lu, number = %d\n", pthread_self(), number);
+    }
+
+    return NULL;
+}
+
+void* funcB_num(void* arg)
+{
+    for(int i=0; i<MAX; ++i)
+    {
+        int cur = number;
+        cur++;
+        number = cur;
+        printf("Thread B, id = %lu, number = %d\n", pthread_self(), number);
+        usleep(5);
+    }
+
+    return NULL;
+}
+# endif
+
+int main(int argc, const char* argv[])
+{
+#ifdef __GNUC__
+    pthread_t p1, p2;
+
+    // 创建两个子线程
+    pthread_create(&p1, NULL, funcA_num, NULL);
+    pthread_create(&p2, NULL, funcB_num, NULL);
+
+    // 阻塞，资源回收
+    pthread_join(p1, NULL);
+    pthread_join(p2, NULL);
+#endif
+    return 0;
+}
+
+
+// 作者: 苏丙榅
+// 链接: https://subingwen.cn/linux/thread-sync/
+// 来源: 爱编程的大丙
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+#endif
