9a837f3ec4c36f85e0975ab99c5650107aa8a405
worker-Windows <yangpan0822@qq.com>
2023-03-14 19:59:00 +0800

    @edit 单例模板基类

--- source/04_DesignPatterns/05_Singleton/main.cpp
+++ source/04_DesignPatterns/05_Singleton/main.cpp
@@ -3,78 +3,162 @@
 2. crtp 单例基类
 */
 
-
 #define TEST1
-
 
 #ifdef TEST1
 
 #include <iostream>
-#include <thread>
 #include <mutex>
+#include <thread>
+#include <vector>
 
 class Singleton
 {
 public:
     static Singleton* GetInstance()
     {
-        static std::once_flag onceFlag;  // 此处onceFlag必须是静态的
-        std::call_once(onceFlag, []()
+        static std::once_flag onceFlag; // 此处onceFlag必须是静态的
+        std::call_once(onceFlag, []() {
+            if (m_instance == nullptr)
             {
-                if (m_instance == nullptr)
-                {
-                    std::cout << "new Singleton\n";
-                    m_instance = new Singleton();
-                }
-            });
+                std::cout << "new Singleton\n";
+                m_instance = new Singleton();
+            }
+        });
         return m_instance;
     }
 
     static void Destroy()
     {
         static std::once_flag onceFlag;
-        std::call_once(onceFlag, []()
+        std::call_once(onceFlag, []() {
+            if (m_instance)
             {
                 std::cout << "destroy Singleton\n";
-                if (m_instance)
-                {
-                    delete m_instance;
-                    m_instance = nullptr;
-                }
-            });
+                delete m_instance;
+                m_instance = nullptr;
+            }
+        });
     }
 
     void Func()
     {
-        std::cout << " === Func()\n";
+        static size_t n = 0;
+        std::cout << " --- " << n++ << " Test: Func()\n";
     }
+
 private:
-    // 静态非const成员必须类外初始化
+    // 静态非const成员必须类外初始化（c++17之前）
     static Singleton* m_instance;
+    Singleton() = default;
+    ~Singleton() = default;
+    Singleton(const Singleton&) = delete;
+    void operator=(const Singleton&) = delete;
+
+    // c++17开始，可以使用inline在类内部初始化静态成员
+    // inline static Singleton* m_instance { nullptr };
 };
 
 Singleton* Singleton::m_instance = nullptr;
 
 int main()
 {
-    // 多次使用GetInstance()，只会new一次
-    Singleton::GetInstance()->Func();
-    Singleton::GetInstance()->Func();
+    std::cout << "--- start ---\n";
 
-    Singleton::Destroy();
+    std::vector<std::thread> threads;
 
-    if (!Singleton::GetInstance())
+    constexpr size_t numOfThreads { 1000 };
+    for (auto i = 0; i < numOfThreads; i++)
     {
-        // 单例类成功析构
-        std::cout << "Instance is nullptr\n";
+        // new Singleton()只会被调用一次
+        std::thread t([]() { Singleton::GetInstance(); });
+        threads.emplace_back(std::move(t));
     }
 
-    // GetInstance()返回值为nullptr还可以访问成员函数，具体请看：00_CPP_TEST->07_Class->TEST9
-    Singleton::GetInstance()->Func();
+    for (auto&& t:threads)
+    {
+        t.join();
+    }
+
+    std::cout << "--- end ---\n";
+
+    return 0;
 }
 
 #endif // TEST1
 
+#ifdef TEST2
 
+#include <iostream>
+#include <mutex>
 
+// crtp https://mp.weixin.qq.com/s/giSDMWLO0d7t3fX-ZC7eVg
 
+template <typename T>
+class SingletonBase
+{
+public:
+    static T* GetInstance() noexcept
+    {
+        static std::once_flag flag;
+        std::call_once(flag, []() {
+            m_instance = new T();
+        });
+        return m_instance;
+    }
+    static void Destroy() noexcept
+    {
+        static std::once_flag flag;
+        std::call_once(flag, []() {
+            if (m_instance)
+            {
+                delete m_instance;
+                m_instance = nullptr;
+            }
+        });
+    };
+
+private:
+    inline static T* m_instance { nullptr };
+
+protected:
+    SingletonBase() noexcept = default;
+    virtual ~SingletonBase() noexcept = default;
+    void operator=(const SingletonBase&) noexcept = delete;
+    SingletonBase(const SingletonBase&) noexcept = delete;
+
+    // 不需要实现参数为右值引用的拷贝构造函数和operator=
+    // void operator=(SingletonBase&&) = delete;
+    // SingletonBase(SingletonBase&&) = delete;
+};
+
+class Derived final: public SingletonBase<Derived>
+{
+    friend class SingletonBase<Derived>;
+
+public:
+    void Test()
+    {
+        std::cout << m_number << '\n';
+    }
+
+private:
+    int m_number { 666 };
+
+private:
+    Derived() noexcept = default;
+    // 同时使用override和noexcept的时候，要保证noexcept在前。
+    ~Derived() noexcept override = default;
+
+    // 拷贝构造和operator=(const SingletonBase&)都会被继承
+};
+
+int main()
+{
+    Derived::GetInstance()->Test();
+    Derived::Destroy();
+
+    return 0;
+}
+
+#endif // TEST2
