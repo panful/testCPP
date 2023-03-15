88059940a530b92b324ae5b59bb1bc42c181e242
worker-Windows <yangpan0822@qq.com>
2023-03-15 14:46:59 +0800

    @edit sizeof(int&)对引用做sizeof

--- source/00_CPP_TEST/11_Sizeof/main.cpp
+++ source/00_CPP_TEST/11_Sizeof/main.cpp
@@ -1,10 +1,10 @@
-﻿//alignof
-//alignas
+﻿// alignof
+// alignas
 
 /*
-* 1. sizeof(基础类型）
-* 2. sizeof(class)
-*/
+ * 1. sizeof(基础类型）
+ * 2. sizeof(class)
+ */
 
 #define TEST2
 
@@ -12,13 +12,14 @@
 
 #include <iostream>
 
-#define SIZE_OF(x) do         \
-{                             \
-std::cout<<"sizeof("          \
-<< #x <<") = \t"              \
-<< sizeof(x)                  \
-<< std::endl;                 \
-}while(false)                 \
+#define SIZE_OF(x)                  \
+    do                              \
+    {                               \
+        std::cout << "sizeof("      \
+                  << #x << ") = \t" \
+                  << sizeof(x)      \
+                  << std::endl;     \
+    } while (false)
 
 int main()
 {
@@ -30,13 +31,13 @@ int main()
     SIZE_OF(long);
     SIZE_OF(long long);
     SIZE_OF(long long int);
-    SIZE_OF(long double);      // 8
+    SIZE_OF(long double); // 8
     SIZE_OF(double);
     SIZE_OF(int);
-    SIZE_OF(unsigned int);     // 4
+    SIZE_OF(unsigned int); // 4
 #ifdef WIN32
     SIZE_OF(unsigned __int64); // 8 Linxu下不能识别 __int64
-#endif // WIN32
+#endif                         // WIN32
     SIZE_OF(size_t);           // Windows = 8;Linux = 8
     SIZE_OF(int64_t);
     SIZE_OF(int32_t);
@@ -62,29 +63,28 @@ int main()
 
 #ifdef TEST2
 
-#include <iostream>
-#include <vector>
 #include <array>
+#include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
-enum class A
-{};
+enum class A {};
 
 enum class AA {
-    ENUM1 = 0x00,              // int
-    ENUM2 = 0xFFF,             // int
-    
+    ENUM1 = 0x00,  // int
+    ENUM2 = 0xFFF, // int
+
     // 报错：枚举值超出int的范围
-    //ENUM3 = 0xFFFFFFFFFFFFFFF, // long long
-    //ENUM4 = 0xFFFFFFFFFFFFFFFF // unsigned long long
+    // ENUM3 = 0xFFFFFFFFFFFFFFF, // long long
+    // ENUM4 = 0xFFFFFFFFFFFFFFFF // unsigned long long
 };
 
 class B
-{};
-
-enum class C:long long
 {
+};
+
+enum class C : long long {
     C1 = 0,
     C2 = 1,
     C3,
@@ -98,7 +98,7 @@ class D
     char b;
 };
 
-class E :public D
+class E : public D
 {
     int c;
     char d;
@@ -106,21 +106,29 @@ class E :public D
 
 class F
 {
-    int arr[10]{ 1,2,3,4,5,6,7,8,9,0 };
-    int* p{ nullptr };
+    int arr[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+    int* p { nullptr };
+};
+
+struct G
+{
+    int a;
+    int& b = a;
+    int& c = a;
+    int& d = a;
 };
 
 int main()
 {
     std::cout << "==================x64====================\n";
 
-    std::cout << sizeof(A) << std::endl; // 枚举无论有几个值都是 4
+    std::cout << sizeof(A) << std::endl;  // 枚举无论有几个值都是 4
     std::cout << sizeof(AA) << std::endl; // 4，即使枚举值为long long，长度仍为4
-    std::cout << sizeof(B) << std::endl; // 空类始终为1
-    std::cout << sizeof(C) << std::endl; // 8，枚举缺省类型是int，指定为long long长度就变为8
-    std::cout << sizeof(D) << std::endl; // 本来为 4+1，1需要和4对齐 所以结果为 4+1+3 = 8
-    std::cout << sizeof(E) << std::endl; // 字节对齐 4+1+3+4+1+3 = 16
-    std::cout << sizeof(F) << std::endl; // 长度为10的数组和int型指针，4*10+8 = 48
+    std::cout << sizeof(B) << std::endl;  // 空类始终为1
+    std::cout << sizeof(C) << std::endl;  // 8，枚举缺省类型是int，指定为long long长度就变为8
+    std::cout << sizeof(D) << std::endl;  // 本来为 4+1，1需要和4对齐 所以结果为 4+1+3 = 8
+    std::cout << sizeof(E) << std::endl;  // 字节对齐 4+1+3+4+1+3 = 16
+    std::cout << sizeof(F) << std::endl;  // 长度为10的数组和int型指针，4*10+8 = 48
 
     std::cout << "==================x64====================\n";
 
@@ -128,30 +136,39 @@ int main()
     std::string str1 = "abc";
     std::string str2 = "1234567890abcd1234567890";
 
-    std::cout << sizeof(std::string) << std::endl;  // 40 std::string内部维护的是一个字符串指针，所以x64下std::string无论存储多少个字符都是40字节
-    std::cout << sizeof(str0) << std::endl;         // 40
-    std::cout << sizeof(str1) << std::endl;         // 40
-    std::cout << sizeof(str2) << std::endl;         // 40
+    std::cout << sizeof(std::string) << std::endl; // 40 std::string内部维护的是一个字符串指针，所以x64下std::string无论存储多少个字符都是40字节
+    std::cout << sizeof(str0) << std::endl;        // 40
+    std::cout << sizeof(str1) << std::endl;        // 40
+    std::cout << sizeof(str2) << std::endl;        // 40
 
     std::cout << "==================x64====================\n";
 
-    std::vector<int> vec1{ 1,2,3 };
-    std::vector<double> vec2{ 1.,2.,3. };
+    std::vector<int> vec1 { 1, 2, 3 };
+    std::vector<double> vec2 { 1., 2., 3. };
 
     std::cout << sizeof(vec1) << std::endl; // 32  vector内部维护的是指针
     std::cout << sizeof(vec2) << std::endl; // 32
 
     std::cout << "==================x64====================\n";
 
-    std::cout << sizeof(std::map<int,double>) << std::endl;    // 24
-    std::cout << sizeof(std::map<double,double>) << std::endl; // 24
-    std::cout << sizeof(std::map<char,char>) << std::endl;     // 24
+    std::cout << sizeof(std::map<int, double>) << std::endl;    // 24
+    std::cout << sizeof(std::map<double, double>) << std::endl; // 24
+    std::cout << sizeof(std::map<char, char>) << std::endl;     // 24
 
     std::cout << "==================x64====================\n";
 
-    std::cout << sizeof(std::array<char,3>) << std::endl;      // 3 std::array<T,Size>内部维护的是一个 T elem[Size]数组，且只有这一个成员（MSVC）
-    std::cout << sizeof(std::array<int,1>) << std::endl;       // 4
-    std::cout << sizeof(std::array<double,2>) << std::endl;    // 16
+    std::cout << sizeof(std::array<char, 3>) << std::endl;   // 3 std::array<T,Size>内部维护的是一个 T elem[Size]数组，且只有这一个成员（MSVC）
+    std::cout << sizeof(std::array<int, 1>) << std::endl;    // 4
+    std::cout << sizeof(std::array<double, 2>) << std::endl; // 16
+
+    std::cout << "==================x64====================\n";
+
+    std::cout << sizeof(G) << '\n';     // 引用本质上是指针，所以是 4 + 4 + 8 * 3，第二个4是为了字节对齐(x64)
+    std::cout << sizeof(int&) << '\n';  // 4
+    std::cout << sizeof(int&&) << '\n'; // 4
+    int number = 1;
+    int& refNumber = number;
+    std::cout << sizeof(refNumber) << '\n'; // 4
 
     return 0;
 }
