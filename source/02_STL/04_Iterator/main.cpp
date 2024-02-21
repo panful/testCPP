549d8bb81a99ecabc12822077515673df0a091e8
worker-Windows <yangpan0822@qq.com>
2024-02-21 11:25:55 +0800

    迭代器常用操作

--- source/02_STL/04_Iterator/main.cpp
+++ source/02_STL/04_Iterator/main.cpp
@@ -1,13 +1,25 @@
-/*
-* 
-*/
+/**
+ * 迭代器 https://zh.cppreference.com/w/cpp/iterator
+ * 1. 迭代器操作 advance distance next prev
+ * std::insert_iterator std::reverse_iterator std::move_iterator std::counted_iterator std::common_iterator
+ */
 
 #define TEST1
 
 #ifdef TEST1
 
+#include <iostream>
+#include <iterator>
+#include <vector>
+
 int main()
 {
-
+    std::vector<int> vec { 0, 1, 2, 3, 4, 5, 6, 7 };
+    auto dist = std::distance(vec.cbegin(), vec.cbegin() + 3);
+    auto iter = vec.begin();      // iter指向0
+    std::advance(iter, 3);        // 将iter向后移动3，iter指向3
+    auto iter2 = std::next(iter); // iter自增，iter指向4
+    auto iter3 = std::prev(iter); // iter自减，iter指向2
 }
+
 #endif // TEST1
--- source/06_Other/05_Optimized/README.md
+++ source/06_Other/05_Optimized/README.md
@@ -4,3 +4,5 @@
 [提高C++性能的编程技术](https://mp.weixin.qq.com/s/SqN8gphEzRNsQMExrFlZxw)
 
 [STL中稍不注意会产生性能开销的地方](https://mp.weixin.qq.com/s/-xHFF3XM2A7FLWW11ijMjg)
+
+[软件优化](https://www.agner.org/optimize/)
