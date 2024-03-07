7996ce2d7156fb249daaeb0dde60453f951c7892
worker-Windows <yangpan0822@qq.com>
2024-03-07 11:37:26 +0800

    标准库时间的使用

--- source/00_CPP_TEST/12_Time/main.cpp
+++ source/00_CPP_TEST/12_Time/main.cpp
@@ -3,11 +3,10 @@
  * 2. C++方式获取当前时间
  * 3. 时间类型转换
  * 4. 计时，秒表
- * 5. 时长，线程睡眠一定时间
- * 6. ++i和i++花费时间
+ * 5. 时间字面量
  */
 
-#define TEST5
+#define TEST2
 
 #ifdef TEST1
 
@@ -70,45 +69,47 @@ int main()
 
 #include <algorithm>
 #include <chrono>
-#include <ctime>
-#include <iomanip>
 #include <iostream>
 
 int main()
 {
-    // 允许用 24h 、 1ms 、 1s 代替对应的,std::chrono::hours(24) 等
-    using namespace std::literals;
+    // 当前时间点，会随着系统时间变化，比如修改Windows的系统时间，这个返回值也会跟着变化
+    // const std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
 
-    // 当前时刻，会随着系统时间变化，比如修改Windows的系统时间，这个返回值也会跟着变化
-    const std::chrono::time_point<std::chrono::system_clock> now = std::chrono::system_clock::now();
+    // 获取当前时间的年月日（注意时区导致的误差）
+    {
+        auto now = std::chrono::system_clock::now();
+        auto tp  = std::chrono::floor<std::chrono::days>(now); // 将当前时间点转换为天数时间点
+        auto ymd = std::chrono::year_month_day(tp);            // C++20
+        auto y   = ymd.year();
+        auto m   = ymd.month(); // 重载了类型转换 unsigned int
+        auto d   = ymd.day();
 
-    const std::time_t t_c = std::chrono::system_clock::to_time_t(now);
+        std::cout << std::format("Year: {} Month: {} Day: {}\n", y, static_cast<uint32_t>(m), d);
+    }
 
-    // 将时间戳转换为longlong类型
-    auto number = static_cast<long long>(t_c);
+    std::cout << "-----------------------------------------\n";
 
-    // int tm_sec;   // seconds after the minute - [0, 60] including leap second
-    // int tm_min;   // minutes after the hour - [0, 59]
-    // int tm_hour;  // hours since midnight - [0, 23]
-    // int tm_mday;  // day of the month - [1, 31]
-    // int tm_mon;   // months since January - [0, 11]
-    // int tm_year;  // years since 1900
-    // int tm_wday;  // days since Sunday - [0, 6]
-    // int tm_yday;  // days since January 1 - [0, 365]
-    // int tm_isdst; // daylight savings time flag
+    // 格式化时间
+    {
+        auto now = std::chrono::system_clock::now();
+        std::cout << std::format("{}\n", now);
+        std::cout << std::format("{:%Y-%m-%d %H:%M:%S}\n", now); // %Y-%m-%d:年月日 %H:%M:%S:时分秒
+        std::cout << std::format("{:%F %T %Z}\n", now);          // %F:年月日 %T:时分秒 %Z:时区缩写
+        std::cout << std::format("{:%OS}\n", now);               // 可以控制秒的精度
+    }
 
-    auto nowTime = std::localtime(&t_c);
-    auto year    = nowTime->tm_year + 1900;
-    auto mon     = nowTime->tm_mon + 1;
-    auto day     = nowTime->tm_mday;
-    auto hour    = nowTime->tm_hour;
-    auto min     = nowTime->tm_min;
-    auto sec     = nowTime->tm_sec;
+    std::cout << "-----------------------------------------\n";
 
-    std::cout << "year:\t" << year << "\nmonth:\t" << mon << "\nday:\t" << day << "\nhour:\t" << hour << "\nminutes:\t" << min << "\nseconds:\t"
-              << sec << '\n';
+    // 时区
+    {
+        auto now               = std::chrono::system_clock::now();
+        auto default_time_zone = std::chrono::current_zone();                     // 默认时区
+        auto local_time        = std::chrono::zoned_time(default_time_zone, now); // 根据时区获取当前时间（本地时间）
 
-    std::cout << std::put_time(nowTime, "%F %T.\n") << std::flush;
+        std::cout << std::format("Current local time: {:%Y-%m-%d %H:%M:%S %Z}\n", local_time);
+        std::cout << std::format("time zone name: {}\n", local_time.get_time_zone()->name());
+    }
 
     return 0;
 }
@@ -117,13 +118,32 @@ int main()
 #ifdef TEST3
 
 #include <chrono>
+#include <format>
 #include <iostream>
 #include <thread>
 
 int main()
 {
-    auto time1 = std::chrono::seconds(5);
-    auto tiem2 = std::chrono::duration_cast<std::chrono::milliseconds>(time1); // 用于将time转换为毫秒
+    // 从一个时长转换到另一个时长，比如：时长1小时转为60分钟
+    {
+        auto time1 = std::chrono::seconds(5);
+        auto tiem2 = std::chrono::duration_cast<std::chrono::milliseconds>(time1); // 将 5s 转换为 毫秒
+        std::cout << std::format("Before: {}\t After: {}\n", time1, tiem2);
+    }
+
+    // 将时间点转换为同一时钟上拥有不同时长的另一个时间点
+    {
+        auto tp1 = std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>(std::chrono::seconds(5));
+        auto tp2 = std::chrono::time_point_cast<std::chrono::milliseconds>(tp1);
+        std::cout << std::format("Before: {}\t After: {}\n", tp1, tp2);
+    }
+
+    // 转换一个时钟的时间点为另一个 C++20
+    {
+        auto tp1 = std::chrono::utc_clock::now();
+        auto tp2 = std::chrono::clock_cast<std::chrono::gps_clock>(tp1);
+        std::cout << std::format("Before: {}\t After: {}\n", tp1, tp2);
+    }
 
     return 0;
 }
@@ -137,8 +157,8 @@ int main()
 
 int main()
 {
+    // 最好不要用这种方式计时，因为 std::system_clock 会随着系统时间的修改而变化
     {
-        // 最好不要用这种方式计时，因为system_clock会随着系统时间的修改而变化
         auto start1 = std::chrono::system_clock::now();
         // do something
         std::this_thread::sleep_for(std::chrono::seconds(3));
@@ -150,8 +170,8 @@ int main()
         auto token2 = end1.time_since_epoch();   // epoch是纪元的意思
     }
 
+    // std::steady_clock 就相当于秒表，所以最好使用该类来计算时间消耗
     {
-        // steady_clock就相当于秒表，所以最好使用该类来计算时间消耗
         // 获取开始时间点
         std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
         // 执行业务流程
@@ -163,9 +183,9 @@ int main()
         std::cout << "total token: " << dt.count() << "ns" << std::endl; // 纳秒
     }
 
+    // std::high_resolution_clock 一般为别名，定义为 std::system_clock 或 std::steady_clock 两者之一
+    // 不推荐使用
     {
-        // high_resolution_clock其实就是steady_clock的别名，源文件中有如下定义
-        // using high_resolution_clock = steady_clock;
         auto start2 = std::chrono::high_resolution_clock::now();
         // do something
         std::this_thread::sleep_for(std::chrono::seconds(3));
@@ -181,76 +201,18 @@ int main()
 #ifdef TEST5
 
 #include <chrono>
+#include <format>
 #include <iostream>
-#include <thread>
-#include <iomanip> // std::put_time
-
-void PrintNowTime()
-{
-    auto tt = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
-    auto lt = std::localtime(&tt);
-    std::cout << "Now: " << std::put_time(lt, "%T") << '\n';
-}
 
 int main()
 {
-    std::cout << "--------------------------------------\n";
+    using namespace std::chrono_literals;
 
-    {
-        PrintNowTime();
-        using namespace std::chrono_literals;                         // 时间字面量
-        std::this_thread::sleep_for(5s);                              // 5s
-        std::this_thread::sleep_for(100ms);                           // 100ms
-        std::this_thread::sleep_for(std::chrono::seconds(3));         // 3s
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // 1000ms = 1s
-        PrintNowTime();
-    }
+    std::chrono::local_days ymd { 2023y / 12 / 17 };
+    std::cout << std::format("Y-M-D: {}\n", ymd);
 
-    std::cout << "--------------------------------------\n";
-
-    {
-        PrintNowTime();
-        auto now = std::chrono::steady_clock::now();
-        std::this_thread::sleep_until(now + std::chrono::seconds(3));
-        PrintNowTime();
-    }
-
-    std::cout << "--------------------------------------\n";
-    return 0;
+    auto hms = 14h + 20min + 10s;
+    std::cout << std::format("H:M:S: {:%T}\tSecond: {}", hms, hms);
 }
+
 #endif // TEST5
-
-#ifdef TEST6
-
-#include <chrono>
-#include <iostream>
-#include <thread>
-
-constexpr size_t count = 10000;
-constexpr double delta = 1000000.0;
-
-int main()
-{
-    auto time1 = std::chrono::high_resolution_clock::now();
-
-    for (auto i = 0; i < count; i++)
-    {
-        std::this_thread::sleep_for(std::chrono::microseconds(1));
-    }
-
-    auto time2 = std::chrono::high_resolution_clock::now();
-
-    for (auto i = 0; i < count; ++i)
-    {
-        std::this_thread::sleep_for(std::chrono::microseconds(1));
-    }
-
-    auto time3 = std::chrono::high_resolution_clock::now();
-
-    auto use1 = std::chrono::duration_cast<std::chrono::microseconds>(time2 - time1).count();
-    auto use2 = std::chrono::duration_cast<std::chrono::microseconds>(time3 - time2).count();
-
-    std::cout << use1 / delta << '\t' << use2 / delta << '\n';
-}
-
-#endif // TEST6
